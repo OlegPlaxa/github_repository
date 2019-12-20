b9b4c2e1bee7e46dd54f92d0e683603c2754c9fa
Oleg Plaxa <plaxaoleg@gmail.com>
2019-12-20 22:15:34 +0300

    Version 5.1.1

--- thank-after-post.php
+++ thank-after-post.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: Thank after post plugin
  * Description: Plugin that adds thank you after all posts
- * Version: 4.1.0
+ * Version: 5.1.1
  * Author: Oleg Plaxa
  */
 add_action( 'the_content', 'thank_after_post' );
 
 function thank_after_post ( $content ) {
-    return $content .= '<p> --Thanks for REVIEW. Updated till 4 version!-- <br>v 4.1.0</p>';
+    return $content .= '<p> --Thanks for REVIEW. Updated till 5 version!-- <br>v 5.1.1</p>';
 }
